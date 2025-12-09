8cc0768b935d60fc8d3ccedf710300f697048a31
Jack Anderson <jack.anderson@salesagility.com>
2025-12-09 13:29:36 +0000

    [Legacy] Add metadata labels

--- public/legacy/modules/DynamicFields/language/en_us.lang.php
+++ public/legacy/modules/DynamicFields/language/en_us.lang.php
@@ -102,4 +102,8 @@ $mod_strings = array(
     'COLUMN_TITLE_INLINE_EDIT_TEXT' => 'Inline Edit',
     'COLUMN_TITLE_PARENT_ENUM' => 'Parent DropDown',
     'LBL_INSERT_FIELD' => 'Insert Field',
+    'LBL_UPLOAD_MAXSIZE' => 'Max Size (in Bytes)',
+    'LBL_STORAGE_TYPE' => 'Storage Type',
+    'LBL_METADATA' => 'Metadata',
+    'LBL_ALLOW_PREVIEW' => 'Allow Preview',
 );

b0113e8965c7c1f95ecb9f4f3cd438d745f9beae
Tapio Kautto <tapio.kautto@evomax.fi>
2026-02-14 21:23:51 +0200

    Fix EmailTemplates attachments detail template paths

--- core/app/core/src/lib/fields/emailtemplates-attachments/templates/detail/emailtemplates-attachments.component.ts
+++ core/app/core/src/lib/fields/emailtemplates-attachments/templates/detail/emailtemplates-attachments.component.ts
@@ -1,9 +1,9 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {EmailTemplatesAttachmentsEditFieldComponent} from '../../edit/emailtemplates-attachments.component';
+import {EmailTemplatesAttachmentsEditFieldComponent} from '../edit/emailtemplates-attachments.component';
 
 @Component({
     selector: 'scrm-emailtemplates-attachments-detail',
-    templateUrl: '../../edit/emailtemplates-attachments.component.html',
+    templateUrl: '../edit/emailtemplates-attachments.component.html',
     styles: [],
     changeDetection: ChangeDetectionStrategy.OnPush
 })

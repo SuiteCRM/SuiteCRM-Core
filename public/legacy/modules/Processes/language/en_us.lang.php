39b49a9775e17d05ac4e8700d51c4c53408ca1a8
Clemente Raposo <clemente.raposo@suitecrm.com>
2026-02-16 12:32:24 +0000

    [Legacy] Add Processes module

---
+++ public/legacy/modules/Processes/language/en_us.lang.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SuiteCRM Ltd.
+ * Copyright (C) 2026 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUITECRM, SUITECRM DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings = [
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified By',
+    'LBL_MODIFIED_NAME' => 'Modified By Name',
+    'LBL_CREATED' => 'Created By',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Name',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_LIST_FORM_TITLE' => 'Processes List',
+    'LBL_MODULE_NAME' => 'Processes',
+    'LBL_MODULE_TITLE' => 'Processes',
+    'LBL_HOMEPAGE_TITLE' => 'My Processes',
+    'LNK_NEW_RECORD' => 'Create Process',
+    'LNK_LIST' => 'View Processes',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Processes',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_NEW_FORM_TITLE' => 'New Process',
+    'LBL_LIST_DELETE' => 'Delete',
+    'LBL_TYPE' => 'Type',
+    'LBL_ESTIMATED_RUN_TIME' => 'Run time estimate',
+    'LBL_STATUS' => 'Status',
+    'LBL_SERVICE_KEY' => 'Service Key',
+    'LBL_LAST_RUN_DATETIME' => 'Last Run DateTime',
+    'LBL_ATTACHMENTS' => 'Attachments',
+    'LBL_OWNER' => 'Owner',
+];
---
+++ public/legacy/modules/Processes/metadata/detailviewdefs.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SuiteCRM Ltd.
+ * Copyright (C) 2026 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUITECRM, SUITECRM DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+$module_name = 'Processes';
+$viewdefs[$module_name]['DetailView'] = [
+    'templateMeta' => [
+        'form' => [
+            'buttons' => [
+            ]
+        ],
+        'maxColumns' => '2',
+        'widths' => [
+            ['label' => '10', 'field' => '30'],
+            ['label' => '10', 'field' => '30']
+        ],
+        'tabDefs' => [
+            'DEFAULT' => [
+                'newTab' => true,
+                'panelDefault' => 'expanded',
+            ],
+        ],
+    ],
+    'header' => [
+        'showFavoritesToggle' => false,
+    ],
+    'recordActions' => [
+        'actions' => [
+            'run-migration' => [
+                'key' => 'run-migration',
+                'labelKey' => 'LBL_RUN_MIGRATION',
+                'asyncProcess' => true,
+                'priority' => 100,
+                'modes' => ['detail'],
+                'params' => [
+                    'asyncProcessKeyField' => 'service_key',
+                    'asyncProcessKeyPrefix' => 'migration-task',
+                    'expanded' => true,
+                    'disableOnRun' => true,
+                    'displayConfirmation' => true,
+                    'confirmationMessages' => ['LBL_RUN_MIGRATION_CONFIRMATION'],
+                ],
+                'displayLogic' => [
+                    'hide-on-running' => [
+                        'modes' => ['detail'],
+                        'params' => [
+                            'activeOnFields' => [
+                                'status' => [
+                                    [
+                                        'operator' => 'not-equal',
+                                        'values' => ['pending']
+                                    ],
+                                ],
+                            ]
+                        ]
+                    ],
+                ],
+            ]
+        ],
+        'exclude' => [
+            'duplicate',
+            'delete',
+            'duplicate-merge',
+            'save',
+            'saveNew',
+            'saveContinue',
+            'edit',
+            'create',
+        ]
+    ],
+    'panels' => [
+        'default' => [
+            [
+                'name',
+                'type',
+            ],
+            [
+                'status',
+                'service_key',
+            ],
+            [
+                'estimated_run_time',
+                'last_run_datetime',
+            ],
+            [
+                'attachments',
+                '',
+            ],
+            [
+                'description',
+                '',
+            ],
+            [
+                [
+                    'name' => 'date_entered',
+                    'label' => 'LBL_DATE_ENTERED',
+                ],
+                [
+                    'name' => 'date_modified',
+                    'label' => 'LBL_DATE_MODIFIED',
+                ],
+            ],
+        ],
+    ],
+];

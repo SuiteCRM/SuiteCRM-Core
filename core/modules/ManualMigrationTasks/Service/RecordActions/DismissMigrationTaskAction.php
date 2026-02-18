9e051e3f44779fe56ee120bf7bb7f90d7d39f67e
Clemente Raposo <clemente.raposo@suitecrm.com>
2026-02-18 13:08:39 +0000

    [Legacy] Add dismiss action to Manual Migration Tasks

---
+++ core/modules/ManualMigrationTasks/Service/RecordActions/DismissMigrationTaskAction.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SuiteCRM Ltd.
+ * Copyright (C) 2026 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUITECRM, SUITECRM DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+namespace App\Module\ManualMigrationTasks\Service\RecordActions;
+
+use ApiPlatform\Metadata\Exception\InvalidArgumentException;
+use App\AsyncTask\Service\Repository\AsyncTaskItemRepository;
+use App\Data\Service\RecordDeletionServiceInterface;
+use App\Module\Service\ModuleNameMapperInterface;
+use App\Process\Entity\Process;
+use App\Process\Service\ProcessHandlerInterface;
+
+class DismissMigrationTaskAction implements ProcessHandlerInterface
+{
+    protected const MSG_OPTIONS_NOT_FOUND = 'Process options are not defined';
+
+    protected const PROCESS_TYPE = 'dismiss-migration-task';
+
+    public function __construct(
+        protected ModuleNameMapperInterface $moduleNameMapper,
+        protected RecordDeletionServiceInterface $recordDeletionProvider,
+        protected AsyncTaskItemRepository $itemRepository
+    ) {
+    }
+
+    public function getProcessType(): string
+    {
+        return self::PROCESS_TYPE;
+    }
+
+    public function requiredAuthRole(): string
+    {
+        return 'ROLE_ADMIN';
+    }
+
+    public function getRequiredACLs(Process $process): array
+    {
+        $options = $process->getOptions();
+        $module = $options['module'] ?? '';
+
+        return [
+            $module => [
+                [
+                    'action' => 'delete',
+                    'record' => $options['id'] ?? ''
+                ]
+            ],
+        ];
+    }
+
+    public function configure(Process $process): void
+    {
+        $process->setId(self::PROCESS_TYPE);
+        $process->setAsync(false);
+    }
+
+    public function validate(Process $process): void
+    {
+        if (empty($process->getOptions())) {
+            throw new InvalidArgumentException(self::MSG_OPTIONS_NOT_FOUND);
+        }
+
+        $options = $process->getOptions();
+
+        if (empty($options['module']) || empty($options['action']) || empty($options['id'])) {
+            throw new InvalidArgumentException(self::MSG_OPTIONS_NOT_FOUND);
+        }
+    }
+
+    public function run(Process $process): void
+    {
+        $options = $process->getOptions();
+        $module = $options['module'] ?? '';
+        $id = $options['id'] ?? '';
+
+        if ($module !== 'manual-migration-tasks') {
+            $process->setStatus('error');
+            $process->setMessages(['LBL_ACTION_ERROR']);
+            return;
+        }
+
+        $this->itemRepository->purgeByTaskId($id);
+
+        $legacyModule = $this->moduleNameMapper->toLegacy($module);
+        $result = $this->recordDeletionProvider->deleteRecord($legacyModule, $id);
+
+        if (!$result) {
+            $process->setStatus('error');
+            $process->setMessages(['LBL_ACTION_ERROR']);
+            return;
+        }
+
+        $process->setStatus('success');
+        $process->setMessages(['LBL_DISMISS_SUCCESS']);
+
+        $responseData = [
+            'handler' => 'redirect',
+            'params' => [
+                'route' => $this->moduleNameMapper->toFrontEnd($module),
+                'queryParams' => []
+            ],
+            'reloadRecentlyViewed' => true,
+        ];
+
+        $process->setData($responseData);
+    }
+}

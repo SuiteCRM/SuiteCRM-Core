b3f16fc299648d4caecd24a6d683dd186f547d0f
Clemente Raposo <clemente.raposo@suitecrm.com>
2026-03-05 23:29:05 +0000

    [Legacy] Move temp dir configuration to suite 8

--- public/legacy/include/utils.php
+++ public/legacy/include/utils.php
@@ -536,7 +536,6 @@ function get_sugar_config_defaults(): array
         'image_field_height_default' => '150px',
         'image_thumbnail_height_default' => 50,
         'image_thumbnail_width_default' => 50,
-        'image_thumbnail_tmp_path' => 'tmp/',
         'valid_image_ext' => [
             'gif',
             'png',

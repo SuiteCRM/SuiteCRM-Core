4359c837dd202596ffedf3dc37ca622d71add592
Jack Anderson <jack.anderson@suitecrm.com>
2026-03-23 11:23:18 +0000

    Add duplicate field mapper registry

---
+++ core/backend/Data/Service/Record/Duplicate/DuplicateFieldMapperRegistry.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SuiteCRM Ltd.
+ * Copyright (C) 2026 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUITECRM, SUITECRM DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+namespace App\Data\Service\Record\Duplicate;
+
+class DuplicateFieldMapperRegistry
+{
+    /**
+     * @var DuplicateFieldMapperInterface[]
+     */
+    protected array $mappers = [];
+
+    public function __construct(iterable $mappers)
+    {
+        foreach ($mappers as $mapper) {
+            $this->mappers[] = $mapper;
+        }
+    }
+
+    /**
+     * Return all mappers that apply to the given module, field type and field name.
+     * Module-specific mappers are included alongside 'default' mappers.
+     * A mapper with a non-null getFieldName() only matches that exact field name.
+     *
+     * @return DuplicateFieldMapperInterface[]
+     */
+    public function getMappers(string $module, string $fieldType, string $fieldName): array
+    {
+        $result = [];
+
+        foreach ($this->mappers as $mapper) {
+            if ($mapper->getModule() !== 'default' && $mapper->getModule() !== $module) {
+                continue;
+            }
+
+            if ($mapper->getFieldType() !== $fieldType) {
+                continue;
+            }
+
+            $mapperFieldName = $mapper->getFieldName();
+            if ($mapperFieldName !== null && $mapperFieldName !== $fieldName) {
+                continue;
+            }
+
+            $result[] = $mapper;
+        }
+
+        return $result;
+    }
+}

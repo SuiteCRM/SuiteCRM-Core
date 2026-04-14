f035973657dc94b92e6be7f32e94ab56d5fcac19
Jack Anderson <jack.anderson@suitecrm.com>
2026-04-14 15:08:49 +0100

    [Legacy] Update survey styling

--- public/legacy/modules/Surveys/Entry/Survey.php
+++ public/legacy/modules/Surveys/Entry/Survey.php
@@ -384,7 +384,7 @@ function displayClosedPage($survey)
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>$survey->name</title>
-        <link href="themes/SuiteP/css/bootstrap.min.css" rel="stylesheet">
+        <link href="themes/suite8/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 EOF;
